ad4353c07ac648e034f37ab755e0723193b195fa
Martin Podloucký <podlouckymartin@gmail.com>
2022-02-10 21:34:30 +0100

    Remove leftover files from compiled sources

--- esm/api-router.d.ts
+++
@@ -1,4 +0,0 @@
-import { Router } from 'express';
-import { ContentIndex } from './index.js';
-declare const createApiRouter: (contentIndex: ContentIndex) => Router;
-export default createApiRouter;
