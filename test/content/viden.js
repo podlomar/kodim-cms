3ab9ff6efab88068835c24ed095a3d9980612808
Martin Podloucký <podlouckymartin@gmail.com>
2022-11-21 21:06:16 +0100

    Add test case for viden content, update rehype-highlight so that it no longer tries to guess the language

---
+++ test/content/viden.js
@@ -0,0 +1,39 @@
+export default {
+  link: 'viden',
+  path: '/kurzy/zemepis/evropa/rakousko/viden',
+  url: 'undefined/content/kurzy/zemepis/evropa/rakousko/viden',
+  title: 'Vídeň (Wien)',
+  repository: {
+    url: 'https://github.com/podlomar/zemepis',
+    branch: 'main',
+    contentPath: 'evropa/rakousko/viden'
+  },
+  crumbs: [
+    { title: 'Zeměpis', path: '/kurzy/zemepis' },
+    { title: 'Evropa', path: '/kurzy/zemepis/evropa' },
+    { title: 'Rakousko', path: '/kurzy/zemepis/evropa/rakousko' }
+  ],
+  status: 'ok',
+  content: {
+    type: 'full',
+    jsml: [
+      [ 'h2', 'Vídeň (', [ 'em', 'Wien' ], ')' ],
+      '\n',
+      [
+        'p',
+        'Vídeň (německy Wien) je hlavní město Rakouska, současně také statutární město a zároveň od 1. ledna 1922 jedna z jeho spolkových zemí, zcela obklopená územím spolkové země Dolní Rakousy. Leží na řece Dunaj a se svými přibližně 1,91 milionu obyvateli je největším rakouským městem a současně nejvýznamnějším politickým, hospodářským a kulturním centrem země.'
+      ],
+      '\n',
+      [ 'h2', 'Historie' ],
+      '\n',
+      [
+        'p',
+        'Historické centrum Vídně bylo v roce 2001 zapsáno na Seznam světového dědictví UNESCO. V celosvětovém seznamu měst podle kvality života sestaveném firmou Mercer v roce 2011 se Vídeň umístila na první příčce. Na zasedání výboru pro Světové dědictví UNESCO, které se konalo v červenci 2017 v Krakově, bylo rozhodnuto o zařazení historického centra Vídně na seznam památek Světového dědictví v ohrožení.'
+      ],
+      '\n',
+      [ 'pre', [ 'code', '.--- .- -.-. .... -.-. .. -.. --- -- ..- -.-.-- ... --- ... -.-.-- -. ..- -.. .. -- ... . -.-.--\n' ] ],
+    ],
+    next: null,
+    prev: null,
+  }
+};
--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ import assert from 'assert';
 import { AccessClaimCheck } from '../esm/content/access-check.js';
 import { KodimCms } from '../esm/index.js';
 import contentTree from './content/tree.js';
+import viden from './content/viden.js';
 
 const cms = await KodimCms.load(
   '/home/podlomar/work/kodim.cz/kodim-cms-test-content',
@@ -14,6 +15,17 @@ const accessCheck = AccessClaimCheck.create({
 
 const rootProvider = cms.getRoot(accessCheck);
 
-it('well formed content tree', () => {
+it('well formed index tree', () => {
   assert.deepStrictEqual(rootProvider.getEntry(), contentTree);
+});
+
+it('well formed section content', async () => {
+  const section = await rootProvider
+    .find('zemepis')
+    .find('evropa')
+    .find('rakousko')
+    .find('viden')
+    .fetch();
+  
+  assert.deepStrictEqual(section, viden);
 });
