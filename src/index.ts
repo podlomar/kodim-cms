de680af6e4d4d419bf20a3a9897744dc5ed35135
Martin Podloucký <podlouckymartin@gmail.com>
2023-08-20 08:40:54 +0200

    Add dedicated domain methods

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import { ContentType } from "filefish/dist/content-types.js";
-import { Cursor, OkCursor } from "filefish/dist/cursor.js";
+import { Cursor } from "filefish/dist/cursor.js";
 import { Filefish, filefish, Asset, FilefishOptions } from "filefish/dist/index.js";
-import { IndexEntry } from "filefish/dist/treeindex.js";
-import { FsNode } from "fs-inquire";
-import { RootEntry, RootContentType } from "./content/root.js";
+import { Chapter, ChapterContentType } from "./content/chapter.js";
+import { Course, CourseContentType } from "./content/course.js";
+import { Exercise, ExerciseContentType } from "./content/exercise.js";
+import { Lesson, LessonContentType } from "./content/lesson.js";
+import { RootEntry, Root, RootContentType } from "./content/root.js";
+import { Section, SectionContentType } from "./content/section.js";
+import { Topic, TopicContentType } from "./content/topic.js";
 
 export class KodimCms {
   private readonly ff: Filefish<RootEntry>;
@@ -20,19 +23,11 @@ export class KodimCms {
     return new KodimCms(ff!);
   }
 
-  public async loadContent<C>(
-    cursor: Cursor, contentType: ContentType<FsNode, IndexEntry, C>
-  ): Promise<C | null> {
-    const content = await this.ff.loadContent(cursor, contentType);
-    if (content === 'forbidden' || content === 'not-found' || content === 'mismatch') {
-      return null;
-    }
+  public async loadAsset(assetPath: string[]): Promise<Asset | null> {
+    const entryPath = assetPath.slice(0, -1);
+    const cursor = this.ff.rootCursor().navigate(...entryPath);
+    const asset = await this.ff.loadAsset(cursor, assetPath.at(-1)!);
 
-    return content;
-  }
-
-  public async loadAsset(cursor: Cursor, assetName: string): Promise<Asset | null> {
-    const asset = await this.ff.loadAsset(cursor, assetName);
     if (asset === 'not-found') {
       return null;
     }
@@ -40,7 +35,101 @@ export class KodimCms {
     return asset;
   }
 
-  public rootCursor(): OkCursor {
-    return this.ff.rootCursor();
+  public async loadRoot(): Promise<Root | null> {
+    const root = await this.ff.loadContent(this.ff.rootCursor(), RootContentType);
+    if (root === 'forbidden' || root === 'not-found' || root === 'mismatch') {
+      return null;
+    }
+
+    return root;
+  }
+
+  public async loadTopic(topicId: string): Promise<Topic | null> {
+    const topic = await this.ff.loadContent(
+      this.ff.rootCursor().navigate(topicId), TopicContentType
+    );
+    
+    if (topic === 'forbidden' || topic === 'not-found' || topic === 'mismatch') {
+      return null;
+    }
+
+    return topic;
+  }
+
+  public async loadCourse(topicId: string, courseId: string): Promise<Course | null> {
+    const course = await this.ff.loadContent(
+      this.ff.rootCursor().navigate(topicId, courseId), CourseContentType,
+    );
+
+    if (course === 'forbidden' || course === 'not-found' || course === 'mismatch') {
+      return null;
+    }
+
+    return course;
+  }
+
+  public async loadChapter(
+    topicId: string, courseId: string, chapterId: string
+  ): Promise<Chapter | null> {
+    const chapter = await this.ff.loadContent(
+      this.ff.rootCursor().navigate(topicId, courseId, chapterId),
+      ChapterContentType,
+    );
+
+    if (chapter === 'forbidden' || chapter === 'not-found' || chapter === 'mismatch') {
+      return null;
+    }
+
+    return chapter;
+  }
+
+  public async loadLesson(
+    topicId: string, courseId: string, chapterId: string, lessonId: string
+  ): Promise<Lesson | null> {
+    const lesson = await this.ff.loadContent(
+      this.ff.rootCursor().navigate(topicId, courseId, chapterId, lessonId),
+      LessonContentType,
+    );
+
+    if (lesson === 'forbidden' || lesson === 'not-found' || lesson === 'mismatch') {
+      return null;
+    }
+
+    return lesson;
+  }
+
+  public async loadSection(
+    topicId: string, courseId: string, chapterId: string, lessonId: string, sectionId: string,
+  ): Promise<Section | null> {
+    const lesson = await this.ff.loadContent(
+      this.ff.rootCursor().navigate(topicId, courseId, chapterId, lessonId, sectionId),
+      SectionContentType,
+    );
+
+    if (lesson === 'forbidden' || lesson === 'not-found' || lesson === 'mismatch') {
+      return null;
+    }
+
+    return lesson;
+  }
+
+  public async loadExercise(
+    topicId: string,
+    courseId: string,
+    chapterId: string,
+    lessonId: string,
+    sectionId: string,
+    exerciseId: string,
+  ): Promise<Exercise | null> {
+    const exercise = await this.ff.loadContent(
+      this.ff.rootCursor().navigate(topicId, courseId, chapterId, lessonId, sectionId, exerciseId),
+      ExerciseContentType,
+    );
+
+    if (exercise === 'forbidden' || exercise === 'not-found' || exercise === 'mismatch') {
+      return null;
+    }
+
+    return exercise;
   }
 }
